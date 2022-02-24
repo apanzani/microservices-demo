7e4b73cac685f82eeb778d7d663dc92abf01ce05
andrea.panzani <andrea.panzani@accenture.com>
2022-02-24 17:03:37 +0100

    aggiunta data e ora

--- docker-compose/check-kafka-topics-created.sh
+++ docker-compose/check-kafka-topics-created.sh
@@ -10,7 +10,8 @@ kafkacatResult=$(kafkacat -L -b kafka-broker-1:9092)
 echo "kafkacat result:" $kafkacatResult
 
 while [[ ! $kafkacatResult == *"twitter-topic"* ]]; do
-  >&2 echo "Kafka topic has not been created yet!"
+  dt=$(date '+%d/%m/%Y %H:%M:%S');
+  >&2 echo "$dt Kafka topic has not been created yet!"
   sleep 10
   kafkacatResult=$(kafkacat -L -b kafka-broker-1:9092)
 done
